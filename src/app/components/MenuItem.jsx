e0fbb702d0439c77745f26498940073bc72501b5
Adukasse <adugnadagnaw01@gmail.com>
2023-09-24 11:06:55 -0500

    created header section

---
+++ src/app/components/MenuItem.jsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import React from "react";
+
+function MenuItem({ title, address, Icon }) {
+  return (
+    <div className="flex">
+      <div className="mx-4 lg:mx-6 ">
+        <Link href={address} className="hover:text-amber-600">
+          <Icon className="text-2xl sm:hidden mx-4" />
+          <p className="hidden sm:inline my-2 text-sm bg-white">{title}</p>
+        </Link>
+      </div>
+
+    
+    </div>
+  );
+}
+
+export default MenuItem;

aa7bdc0f3fab68e2ed608c06805f89a166e3f07b
Adukasse <adugnadagnaw01@gmail.com>
2023-09-26 11:57:49 -0500

    created about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+function page() {
+  return (
+    <div className=" max-w-6xl mx-auto space-y-4">
+      <h1 className=" text-2xl font-medium text-amber-300 m-6">
+        About{" "}
+      </h1>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error
+        perferendis debitis reiciendis dolorem officia? Omnis, unde autem
+        obcaecati suscipit atque accusamus error cumque provident, magnam, vitae
+        eos adipisci voluptatibus praesentium
+      </p>
+      <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error
+        perferendis debitis reiciendis dolorem officia? Omnis, unde autem
+        obcaecati suscipit atque accusamus error cumque provident, magnam, vitae
+        eos adipisci voluptatibus praesentium
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error
+        perferendis debitis reiciendis dolorem officia? Omnis, unde autem
+        obcaecati suscipit atque accusamus error cumque provident, magnam, vitae
+        eos adipisci voluptatibus praesentium
+      </p>
+    </div>
+  );
+}
+
+export default page;
